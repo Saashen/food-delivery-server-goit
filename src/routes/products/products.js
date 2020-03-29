111eb355eb4ddd756b0e895b425ab4b5f654adfd
Saashen <saashen.kyiv@gmail.com>
2020-03-29 20:17:51 +0300

    hw 01

---
+++ src/routes/products/products.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const path = require('path');
+
+const productsRoute = (request, response) => {
+  const filePath = path.join(__dirname, '../../', 'db', 'all-products.json');
+  response.writeHead(200, { 'Content-Type': 'application/json' });
+
+  const readStream = fs.createReadStream(filePath);
+  readStream.pipe(response);
+};
+
+module.exports = productsRoute;
