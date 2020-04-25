4591e0ca0f85a8bbd7027833ba49f56ce1311d6f
Saashen <saashen.kyiv@gmail.com>
2020-04-25 08:27:06 +0300

    product in process

--- src/routes/products/product.js
+++ src/routes/products/product.js
@@ -1,16 +1,40 @@
-const getUserFromDb = id => {
-  const src = path.resolve(usersFolder, fileName + '.json');
+const path = require('path');
+var fs = require('fs');
 
-  return readFileSync(src);
+const getProductFromDb = id => {
+  const filePath = path.join(__dirname, '../../', 'db', 'all-products.json');
+
+  return fs.readFile(filePath, 'utf8', function (err, data) {
+    if (err) throw err;
+    const readedFile = JSON.parse(data);
+    const productWithId = readedFile.find(product => product.id === id);
+
+    return JSON.stringify(productWithId);
+  });
 };
 
 const getProduct = (request, response) => {
-  const id = request.params.userId;
+  const id = request.params.id;
 
-  response.set('Content-Type', 'application/json');
+  const sendResponse = product => {
+    response.status(200);
+    response.set('Content-Type', 'application/json');
+    response.json({
+      product,
+    });
+  };
 
-  response.status(200);
-  response.json({ user: getUserFromDb(id) });
+  const sendError = () => {
+    response.set('Content-Type', 'application/json');
+    response.status(400);
+    response.json({
+      error: 'user was not saved',
+    });
+  };
+
+  getProductFromDb(id)
+    .then(data => sendResponse(data))
+    .catch(sendError);
 };
 
 module.exports = getProduct;
